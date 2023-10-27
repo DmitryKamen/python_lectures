4cee5c1d4def1948dc20d91d3c0d334b2d920b2e
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-10-27 13:11:31 +0200

    Добавили .folder и main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+"""
+print(1)
+n = int(input())
+print(n)
+"""
+# n = 5
+# print(n)
+
+# m = None 
+
+# s = 'tex'
+# s1 = "text"
+
+# n1 = 5
+# print(type(n1))
+
+# a = 5
+# b = 5.89
+# c = 'hellow'
+# # print(a,b,c)
+# # print(a,'-',b,'-',c)
+# # print(f"{a}-{b}-{c}")
+# print("{} - {} - {}".format(a,b,c))
+
+# # input()
+# print('enter string 1: ')
+# n = input()
+# # print(n)
+# m = input('enter string 2: ')
+# print(n,' + ',m,' = ', n + m)
+
+# c = 5.8
+# print(c)
+# print(type(c))
+# c =int(c)
+# print(c)
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c =bool(c)
+# print(c)
+# print(type(c))
+
+
+# print('enter string 1: ')
+# n = int(input())
+# m = int(input('enter string 2: '))
+# print(n,' + ',m,' = ', n + m)
+
+a = 5.89956
+b = 6.556551
+# print(a*b)
+print(round (a*b,3))
+
